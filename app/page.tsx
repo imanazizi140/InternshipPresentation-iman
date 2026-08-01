410d0e339abf3caab5fc2385dc78a9245e818bbf
imanazizi140-sudo <imanazizi140@gmail.com>
2026-08-01 16:37:08 +0800

    Embed Canva presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { PresentationDeck } from "@/components/presentation-deck"
-
 export default function Page() {
-  return <PresentationDeck />
+  return (
+    <main className="canva-presentation">
+      <iframe
+        title="Internship presentation"
+        src="https://www.canva.com/design/DAHQ8h6Nt44/_jwGoUQSQUQzRkYTuLjV_Q/view?embed"
+        allowFullScreen
+        allow="fullscreen"
+      />
+    </main>
+  )
 }
